741ef8d82cbd469746dec09a4392e5c62eac5c07
Morgan <Morganvallen@gmail.com>
2017-06-19 13:43:03 -0400

    changed message

--- linkedinjsv2.js
+++ linkedinjsv2.js
@@ -24,24 +24,24 @@
 //         $('.compose-txtarea').val("\
 // Hey "+first_name+",\n\
 // \n\
-// I am on the recruiting team at NerdWallet and wanted to see if you might be open to a role on our team. We are a Series A startup with 10's of millions of users and generating $100MM+ in revenue.\n \
+// Add some text to a message here\n \
 // \n\
-// We are working to be the trusted source for helping consumers make the best financial decisions. We're transitioning from a content driven marketplace to a data driven one. We're in the early stages of collecting information by registering our members, linking their credit score and financial accounts and then looking after their financial needs proactively. We are being very proactive about any security concerns and building the team before any issues arise.\n \
+// To create new lines \n \
 // \n\
-// I thought you could be a great fit for a Senior level role on our Security team. You'd be working very closely with our Senior Director of Security (https://www.linkedin.com/in/balasathiamurthy/) on building out our Security program. A few things that you would be responsible for in this role:\n \
+// Can create bullet points:\n \
 // \n \
-//     -Build creative tools and solutions to security-harden NerdWallet’s production infrastructure including systems and networking without diminishing productivity\n \
+//     -Like this if you want!\n \
 //     \n\
-//     -Build security tools and monitoring capabilities to improve our ability to detect security incidents and response capabilities.\n \
+//     -Second bullet point\n \
 //     \n\
-//     -Be a go-to security expert that all team members can learn from, and evangelize NerdWallet's own security culture\n \
+//     -Third bullet point\n \
 // \n\
 // ...and so much more!\n \
 // \n\
-// Let me know if are interested, I'd certainly like to tell you more about us! Thanks "+first_name+". \n \
+// Add some more text! Thanks "+first_name+". \n \
 // \n\
 // Best,\n \
-// Morgan"
+// SOME NAME"
 //         )
 
 //     }, 2000)
